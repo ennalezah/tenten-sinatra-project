16e3908ab0bc8830f0b66e800c232536b9da95a8
ennalezah <hae.villareal@gmail.com>
2019-10-02 20:47:33 -0700

    Added validations and association to model Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: { message: "Title cannot be blank" }
+
+  validates :description, presence: { message: "Description cannot be blank" }
+  
   belongs_to :user
 end
