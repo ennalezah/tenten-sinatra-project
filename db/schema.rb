f7920db8b0e965c1fbe0115c3e160e8edd00c515
ennalezah <hae.villareal@gmail.com>
2019-10-06 16:13:04 -0700

    Added column to posts table

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191005075621) do
+ActiveRecord::Schema.define(version: 20191006231044) do
 
   create_table "posts", force: :cascade do |t|
     t.string  "title"
     t.text    "description"
     t.string  "url"
     t.integer "user_id"
+    t.string  "category"
   end
 
   create_table "users", force: :cascade do |t|
