43e2a427f922f7f2b78d9af3d29ae0b07fb635a3
ennalezah <hae.villareal@gmail.com>
2019-10-07 00:18:42 -0700

    Added post validation and updated post_error.erb

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,11 @@
 class Post < ActiveRecord::Base
   belongs_to :user
 
-  validates :category, exclusion: { in: %w(Please select one), message: "Choose the category that best describes your post." }
+  validates :category, exclusion: { in: %w(select), message: "Choose the category that best describes your recommendation." }
   
   validates :title, presence: { message: "Title cannot be left blank." }
 
   validates :description, presence: { message: "Description cannot be left blank." }
 
-  validates :url, presence: { message: "Link cannot be left blank." }
+  validates :url, presence: { message: "Link cannot be left blank and must have the correct format (https://thisisanexample.com)." }
 end
