af701b4fa5d3e8e767b19f994416e2691fb63a53
ennalezah <hae.villareal@gmail.com>
2019-10-09 22:37:22 -0700

    Added styling to index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,15 @@
 class UsersController < ApplicationController
   get '/users/:slug' do
     @user = User.find_by_slug(params[:slug])
-    erb :'/users/profile'
+
+    if @user      
+      erb :'/users/profile'
+    else
+      erb :'/users/profile_not_found'
+    end
   end 
+
+  get '/users/profile-not-found' do
+    erb :'/users/profile_not_found'
+  end
 end
